1e410b180a50fe6d6a59744f23f4c19f55f927a3
PisethChuon <122347263+PisethChuon@users.noreply.github.com>
2024-03-19 23:56:18 +0700

    db-test

--- db-demo/routes/web.php
+++ db-demo/routes/web.php
@@ -4,16 +4,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
-// Rean CRUD
-Route::get('/items', [ItemController::class, 'index']);
-Route::get('/items/create', [ItemController::class, 'create']);
-Route::post('/items', [ItemController::class, 'store']);
-Route::get('/items/{id}', [ItemController::class, 'show']);
-Route::get('/items/{id}/edit', [ItemController::class, 'edit']);
-Route::put('/items/{id}', [ItemController::class, 'update']);
-Route::delete('/items/{id}', [ItemController::class, 'destroy']);
-// Rean CRUD
-
 Route::get('/', function () {
     return view('welcome');
 });
