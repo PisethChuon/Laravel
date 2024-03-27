1710842e4fb0d526ac7a34905f8927753cc80395
PisethChuon <122347263+PisethChuon@users.noreply.github.com>
2024-03-27 23:24:13 +0700

    laravel-crud

--- laravel-crud/app/Http/Controllers/StudentController.php
+++ laravel-crud/app/Http/Controllers/StudentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
  
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
-use App\Models\Student; //add Student Model - Data is coming from the database via Model.
+use App\Models\Student; 
  
 class StudentController extends Controller
 {
