0460fc62fcc31706075facd918755aa4285ba376
PisethChuon <122347263+PisethChuon@users.noreply.github.com>
2024-03-27 12:08:04 +0700

    laravel-crud

--- laravel-crud/routes/web.php
+++ laravel-crud/routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', [TodoController::class, 'index']);
 Route::get('create', [TodoController::class, 'create']);
 Route::get('details/{todo}', [TodoController::class, 'details']);
 Route::get('edit/{todo}', [TodoController::class, 'edit']);
+Route::post('update/{todo}', [TodoController::class, 'update']);
 Route::post('store-data', [TodoController::class, 'store']);
 
 
