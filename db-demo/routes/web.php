2d647cd2c2e2cf91cad6f9dd863303828d054c01
PisethChuon <122347263+PisethChuon@users.noreply.github.com>
2024-03-28 12:24:06 +0700

    db-demo

--- db-demo/routes/web.php
+++ db-demo/routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,7 +16,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-
 
 require __DIR__.'/auth.php';
