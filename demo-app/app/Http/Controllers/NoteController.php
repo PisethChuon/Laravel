b14bd259aa393c8d52ea0f4f8b8b0c53bf0ce700
PisethChuon <122347263+PisethChuon@users.noreply.github.com>
2024-05-09 00:40:42 +0700

    demo-app

--- demo-app/app/Http/Controllers/NoteController.php
+++ demo-app/app/Http/Controllers/NoteController.php
@@ -12,7 +12,10 @@ class NoteController extends Controller
      */
     public function index()
     {
-        return view('note.index');
+        
+        $notes  = Note::query()->orderBy('create_at', 'desc')->paginate();
+        // dd($notes);
+        return view('note.index', ['notes' => $notes]);
     }
 
     /**
@@ -37,7 +40,7 @@ class NoteController extends Controller
      */
     public function show(Note $note)
     {
-        return view('note.show');
+        return view('note.show', ['note' => $note]);
     }
 
     /**
@@ -45,7 +48,7 @@ class NoteController extends Controller
      */
     public function edit(Note $note)
     {
-        return view('note.edit');
+        return view('note.edit', ['note' => $note]);
     }
 
     /**
--- demo-app/resources/views/components/layout.blade.php
+++ demo-app/resources/views/components/layout.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.3/base.min.css" rel="stylesheet">
     
 </head>
 <body>
