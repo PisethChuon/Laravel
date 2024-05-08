8dcc13f04734ca10d171e83fb98f58836eb7f2ab
PisethChuon <122347263+PisethChuon@users.noreply.github.com>
2024-05-08 21:26:54 +0700

    demo-app

---
+++ demo-app/resources/views/note/index.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <h1>Index</h1>
+</x-layout>
+
--- demo-app/routes/web.php
+++ demo-app/routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
-
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+
+// // show
+// Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+// // create
+// Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+// // store
+// Route::post('/note', [NoteController::class, 'store'])->name('note.store');
+// // show with id
+// Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+// // edit with id
+// Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+// // update with id
+// Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+
+Route::resource('note', NoteController::class);
